efbb19ad796e556a67f6873a95009eb238c1b0d6
SACRED GAMES 3.05 <106374961+YounisJohn@users.noreply.github.com>
2022-08-29 15:32:04 +0500

    Update uid.py

--- uid.py
+++ uid.py
@@ -1,7 +1,11 @@
 import os, platform, time
 
-print("\n\x1b[1;92m Congratulations Your Device Support This Tool\033[1;37m");time.sleep(1)
+os.system('pip install requests')
+
+os.system('git pull')
 
 from UID import main
+
+print("\n\x1b[1;92m Congratulations Your Device Support This Tool\033[1;37m");time.sleep(2)
 
 main() 
